9655ac96dac32f4f435050ad312cd7dab2a19e62
Ruth Njaramba <rnjaramba@gmail.com>
2026-02-19 10:15:53 +0300

    Remove BrowserRouter from index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -13,9 +12,6 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Navicate-GOOGLE">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
-
