871de1806749839637a2fc49cae773af68296f15
Ruth Njaramba <rnjaramba@gmail.com>
2026-02-24 15:55:03 +0300

    Replace React app with static HTML content
    Removed React rendering logic and added a simple HTML structure.

--- index.tsx
+++ index.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Test Page</title>
+</head>
+<body>
+  <h1>Test</h1>
+  <p>If you see this, GitHub Pages is working. Your React app needs to be built.</p>
+</body>
+</html>
